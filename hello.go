8897bcbff9444467bebc36b8fae6d5604b8d0a99
shradiphylleia <shradulsha@gmail.com>
2026-05-08 16:53:39 +0530

    Task 1: reading from a file 8 bytes a time

--- hello.go
+++ hello.go
@@ -1,13 +1,16 @@
 package main
 
 // print library
-import ("fmt" "math" "strings")
+import (
+	"fmt" 
+	"math"
+ 	"strings")
 
 
 // go primer:
 
 // package level variable
-
+// vars: =: short variable declaration operator, only func outside vars and type , auto infers the type
 
 // constants
 
@@ -46,3 +49,28 @@ func main(){
 
 
 
+// os package : interact with os platform independent interface 
+// open files , mkdir read env vars , get process info , delete files, handle stdin/stdout
+// type-assert errors *os.PathError , extract structured info from the error
+
+// if we create something we MUST use it.
+// unsused variables as compile errors
+// go automatically inserts semicolons at line endings so else must be on the same line as the closing of the if block
+// idiomatic go usually avoid else: err path exists early , success path clean and less nesting
+// The methods of File correspond to file system operations. All are safe for concurrent use. The maximum number of concurrent operations on a File may be limited by the OS or the system. The number should be high, but exceeding it may degrade performance or cause other issues.
+
+
+// file gave hash: location ? : different value
+// how to get params?
+// *file: different values
+
+
+// reading bytes: 
+// []byte : slice of bytes
+// make would create an underlying array and slice pointing to it
+// data := make ([]byte, 8)
+// slice is dynamic view of bytes [] sequence of bytes
+// go zero initializes memory by default, each element is a byte (uint8) so 0 = 00000000 in binary
+
+// only for 
+// for {} infinte loop
--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import "fmt"
-
-
+import ("fmt" 
+		"os"
+ 		"log")
 
 func main(){
-	fmt.Println("namastute")
+
+	// open
+	file, err := os.Open("messages.txt")
+	if err != nil {
+	log.Fatal (err)
+	}
+	fmt.Println("file opened", file.Name())
+
+	// read : while err not show up
+	for{
+	data := make ([]byte, 8)
+	count , err := file.Read(data)
+	if err != nil{
+		break
+	}
+	fmt.Printf( "read: %s\n", string(data[:count]))
+
+	}
 }
+
