d12330e31d093264061f4dee454f0d9fd7568939
shradiphylleia <shradulsha@gmail.com>
2026-05-08 13:59:58 +0530

    go primer

---
+++ hello.go
@@ -0,0 +1,39 @@
+package main
+
+// print library
+import ("fmt" "math" "strings")
+
+
+// go primer:
+
+// package level variable
+
+
+// constants
+
+const (
+	name = "shraddha"
+	msg = "no cap"
+	day = "friday"
+)
+
+
+func main(){
+	var x int = 400
+	var y int = 60
+	var z int = 4
+	// var x,y,z int =400,60,4
+
+	// x, y, z :=400, 60, 4
+}
+
+// has pointer as a concept also
+// rae strings in backticks
+// int float64 bool string var ptr *int [nil]
+// Printf, Sprintf- formatted output
+
+// type aliases
+
+
+
+
