12b3b6e76676bca0d300d51d7bdf1e12060da0f3
shradiphylleia <shradulsha@gmail.com>
2026-06-05 11:23:13 +0530

    state machine

--- http/internal/request/request.go
+++ http/internal/request/request.go
@@ -1,18 +1,20 @@
 package request
 
 import (
-	"io"
 	"errors"
+	"io"
 	"strings"
+)
+
+const (
+	stateInitialized = iota
+	stateDone
 )
 
 type Request struct {
 	RequestLine RequestLine
+	state       int
 }
-
-// GET /coffee HTTP/1.1
-// /coffee requestTarget 
-// method GET
 
 type RequestLine struct {
 	HttpVersion   string
@@ -20,32 +22,60 @@ type RequestLine struct {
 	Method        string
 }
 
-// Create a request from a reader
-// io.Reader: HTTP doesn't care where bytes come from.
-func RequestFromReader(reader io.Reader) (*Request, error){
-
+func RequestFromReader(reader io.Reader) (*Request, error) {
 	body, err := io.ReadAll(reader)
-	if err!= nil{
+	if err != nil {
 		return nil, err
 	}
-
-	reqText := string (body)
-	lines := strings.Split(reqText, "\r\n")
-
-	requestLine, err :=parseRequestLine(lines[0])
-
-	if err!= nil{
-		return nil , err
+	req := &Request{
+		state: stateInitialized,
 	}
-
-	return &Request{RequestLine: requestLine} , nil
+	_, err = req.parse(body)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
 }
 
-func parseRequestLine(line string) (RequestLine, error){
-	components := strings.Split(line, " ")
-	if len(components)!= 3 || strings.ToUpper(components[0]) != components[0] || (components[2])!= "HTTP/1.1" {
-		return RequestLine{}, errors.New("invalid request line")
+func parseRequestLine(data []byte) (RequestLine, int, error) {
+	idx := strings.Index(string(data), "\r\n")
+	// need more data
+	if idx == -1 {
+		return RequestLine{}, 0, nil
 	}
-		version := strings.Split(components[2], "/")
-		return RequestLine{HttpVersion: version[1], RequestTarget: components[1], Method: components[0]} , nil
+	line := string(data[:idx])
+	components := strings.Split(line, " ")
+	if len(components) != 3 ||
+		strings.ToUpper(components[0]) != components[0] ||
+		components[2] != "HTTP/1.1" {
+		return RequestLine{}, 0, errors.New("invalid request line")
+	}
+	version := strings.Split(components[2], "/")
+	requestLine := RequestLine{
+		HttpVersion:   version[1],
+		RequestTarget: components[1],
+		Method:        components[0],
+	}
+	// consumed request-line+CRLF
+	return requestLine, idx+2, nil
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	switch r.state {
+	case stateInitialized:
+		requestLine, consumed, err := parseRequestLine(data)
+		if err != nil {
+			return 0, err
+		}
+		// parser needs more bytes
+		if consumed == 0 {
+			return 0, nil
+		}
+		r.RequestLine = requestLine
+		r.state = stateDone
+		return consumed, nil
+	case stateDone:
+		return 0, nil
+	}
+	return 0, nil
 }
